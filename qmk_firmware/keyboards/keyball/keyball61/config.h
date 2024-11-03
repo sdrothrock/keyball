b737835b4236c5c9b593d0ed820ad478e827ec09
Scott Rothrock <scott@blackwhitedog.com>
2024-11-03 22:02:37 +0900

    Add layer light config

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -63,6 +63,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_MATRIX_SPLIT    { 37, 37 }
 #endif
 
+#define RGBLIGHT_LAYERS
+
 #ifndef OLED_FONT_H
 #    define OLED_FONT_H "keyboards/keyball/lib/logofont/logofont.c"
 #    define OLED_FONT_START 32
