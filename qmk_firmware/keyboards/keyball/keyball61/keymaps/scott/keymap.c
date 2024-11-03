d5d86c99ead8838b4a8003c19aac7de84083a7c1
Scott Rothrock <scott@blackwhitedog.com>
2024-11-03 22:01:25 +0900

    Fix punctuation, add trackball config keys

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/scott/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/scott/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     LT(2, KC_LSFT), KC_Z, KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-    KC_LALT  , KC_DEL   , KC_LGUI  , XXXXXXX  ,LT(4, KC_LNG2),LT(3,KC_SPC), KC_ENT ,        LT(4,KC_LNG1),LT(1,KC_LBRC),                               KC_INT3  , KC_EQL
+    KC_LALT  , KC_DEL   , KC_LGUI  , XXXXXXX  ,LT(4, KC_LNG2),LT(3,KC_SPC), KC_ENT ,        LT(4,KC_LNG1),LT(1,KC_LBRC),                           KC_INT3  , KC_EQL
   ),
   // immediately above lower layer so that almost all keys can be transparent for immediate typing
   [_MOUSE] = LAYOUT(
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  ,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2, _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , SCRL_MO  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  ,S(KC_LBRC),                                  _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  ,                                  _______  , _______
   ),
   [_UPPER] = LAYOUT(
-    S(KC_ESC) ,S(KC_1)  , S(KC_2)   ,S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)   ,S(KC_MINS),
+    S(KC_ESC) ,S(KC_1)  , S(KC_2)   ,S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_EQL) ,S(KC_MINS),
     S(KC_TAB) ,S(KC_Q)  , S(KC_W)   ,S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)   ,S(KC_BSPC),
     S(KC_LCTL),S(KC_A)  , S(KC_S)   ,S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , S(KC_SCLN),S(KC_QUOT),
     _______   ,S(KC_Z)  , S(KC_X)   ,S(KC_C)  , S(KC_V)  , S(KC_B)  , S(KC_RBRC),         S(KC_NUHS),  S(KC_N)  , S(KC_M)  , S(KC_COMM),S(KC_DOT), S(KC_SLSH),S(KC_INT1),
-    S(KC_LALT),S(KC_DEL), S(KC_LGUI),LSG(KC_S), _______  ,LT(2,KC_SPC),S(KC_ENT),           _______ ,    _______,                                  S(KC_INT3),S(KC_EQL)
+    S(KC_LALT),S(KC_DEL), S(KC_LGUI),LSG(KC_S), _______  ,LT(2,KC_SPC),S(KC_ENT),           _______ ,  S(KC_LBRC),                                 S(KC_INT3),S(KC_EQL)
   ),
   [_UTILS] = LAYOUT(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_SETUP] = LAYOUT(
     QK_BOOT  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , QK_BOOT  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , SCRL_DVD , KBC_SAVE , _______  , SSNP_VRT ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , CPI_D100 , SCRL_DVI , CPI_I100 , _______  , SSNP_FRE ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , RGB_TOG  ,            RGB_TOG  , _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  ,                                  _______  , _______
   ),
@@ -89,7 +89,7 @@ void oledkit_render_info_user(void) {
 
 #if defined(RGBLIGHT_ENABLE)
 #define LOWER_LIGHT_BLUE    175,   0,  80
-#define MOUSE_ORANGE         21, 255, 255
+#define MOUSE_ORANGE         30, 255, 255
 #define UPPER_BLUE          175, 255,  80
 #define UTILS_BLUE          175, 140,  80
 #define SETUP_RED             0, 255, 255
